43773e9bc53ef8ac947648b0dc0405d597c58dde
Tesfatsion-Gudeta <tesfatsiongudeta7@gmail.com>
2024-09-16 18:21:16 +0300

    conneting to db and routes

---
+++ index.js
@@ -0,0 +1,25 @@
+const express=require('express')
+const mongoose=require('mongoose')
+require('dotenv').config()
+const app=express()
+const writings=require('./routes/writings')
+const qoutes=require('./routes/qoutes')
+
+
+
+app.use('/api/writings',writings)
+app.use('/api/qoutes',qoutes)
+
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(()=>{
+    console.log('mongodb connected succesfully.')
+  })
+  .catch(err=>{console.error('mongodb connection error: ',err)})
+
+  // Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
