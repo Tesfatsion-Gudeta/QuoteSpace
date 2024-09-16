8ce9ce97e96ce852674d9b5d3e8f2115b87a9842
Tesfatsion-Gudeta <tesfatsiongudeta7@gmail.com>
2024-09-16 18:15:29 +0300

    qoute model and route handler files

---
+++ models/qoute.js
@@ -0,0 +1,33 @@
+const mongoose=require('mongoose')
+const Joi=require('joi')
+
+const qouteSchema=new mongoose.Schema({
+    text:{
+        type:String,
+        minLength:20,
+        maxLength:1000,
+        required:true
+    },
+    reference:{
+        type:String,
+        required:true,
+        
+    }
+})
+
+const Qoute=mongoose.model('Qoute',qouteSchema)
+
+//input validation
+
+function validateQoute(qoute){
+    const schema=Joi.object({
+        text:Joi.string().min(20).max(1000).required(),
+        reference:Joi.string().min(5).max(200).required()
+    })
+    return schema.validate(qoute)
+}
+
+//export
+
+exports.Qoute=Qoute
+exports.validate=validateQoute
