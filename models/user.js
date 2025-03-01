bf94fff21d8e5acf4a3a9ef410e350291ef9a622
tesfatsion <tesfatsiongudeta7@gmail.com>
2025-03-01 00:31:40 -0800

    edit environment varibable handling

--- models/user.js
+++ models/user.js
@@ -1,64 +1,65 @@
-const mongoose=require('mongoose')
-const Joi=require('joi')
-const jwt=require('jsonwebtoken')
-const config=require('config')
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const jwt = require("jsonwebtoken");
+// const config = require("config");
 
-const userSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        minLength:3,
-        maxLength:50,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-        minLength:5,
-        maxLength:255
-    },
-    password:{
-        type:String,
-        required:true,
-        minLength:6,
-        maxLength:1024
-    },
-    isAdmin:{
-        type:Boolean,
-        default:false
-    }
-})
+require("dotenv").config();
+const jwtPrivateKey = process.env.JWTPRIVATEKEY;
+const jwtRefreshKey = process.env.JWTREFRESHKEY;
 
-
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minLength: 3,
+    maxLength: 50,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    minLength: 5,
+    maxLength: 255,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+    maxLength: 1024,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+});
 
 //generating jsonwebtoken for signedup users
-userSchema.methods.generateAuthToken=function(){
-    const token=jwt.sign({_id:this._id},config.get('jwtPrivateKey'))
-    return token
-}
+userSchema.methods.generateAuthToken = function () {
+  //   const token = jwt.sign({ _id: this._id }, config.get("jwtPrivateKey"));
+  const token = jwt.sign({ _id: this._id }, jwtPrivateKey);
+  return token;
+};
 
+userSchema.methods.generateRefreshToken = function () {
+  //   const token = jwt.sign({ _id: this._id }, config.get("jwtRefreshKey")
+  const token = jwt.sign({ _id: this._id }, jwtRefreshKey, {
+    expiresIn: "7d",
+  });
+  return token;
+};
 
-
-userSchema.methods.generateRefreshToken=function(){
-    const token=jwt.sign({_id:this._id},config.get('jwtRefreshKey'),{expiresIn:'7d'})
-    return token
-}
-
-
-const User=mongoose.model('User',userSchema)
-
+const User = mongoose.model("User", userSchema);
 
 //input validation
-function validateUser(user){
-    const schema=Joi.object({
-        name:Joi.string().min(3).max(50).required(),
-        email:Joi.string().min(5).max(255).required().email(),
-        password:Joi.string().min(6).max(255).required(),
-        isAdmin: Joi.boolean()
-    })
-       return schema.validate(user)
-    
+function validateUser(user) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(50).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(6).max(255).required(),
+    isAdmin: Joi.boolean(),
+  });
+  return schema.validate(user);
 }
-    
-exports.User=User
-exports.validate=validateUser
+
+exports.User = User;
+exports.validate = validateUser;
--- routes/users.js
+++ routes/users.js
@@ -1,111 +1,105 @@
-const express=require('express')
-const mongoose=require('mongoose')
-const {validate,User}=require('../models/user')
-const _=require('lodash')
-const bcrypt=require('bcrypt')
-const router=express.Router()
-const auth=require('../middleware/auth')
-// const { config } = require('dotenv')
-const config=require('config')
-const jwt=require('jsonwebtoken')
-
+const express = require("express");
+const mongoose = require("mongoose");
+const { validate, User } = require("../models/user");
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
+const router = express.Router();
+const auth = require("../middleware/auth");
+require("dotenv").config();
+// const config=require('config')
+const jwt = require("jsonwebtoken");
+const jwtRefreshKey = process.env.JWTREFRESHKEY;
 
 //routes
-router.get('/me',auth,async(req,res)=>{
-   const user= await User.findById(req.user._id).select('-password')
-   res.send(user)
-
-})
-
+router.get("/me", auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password");
+  res.send(user);
+});
 
 //signin
-router.post('/signin',async(req,res)=>{
-   const {error}=validate(req.body)
-   if(error) return res.status(400).send(error.details[0].message)
-   
-   let user=await User.findOne({email:req.body.email})
-    if(user) return res.status(400).send('user already registered')
-    
-    user=new User(_.pick(req.body,['name','email','password']))
-    const salt=await bcrypt.genSalt(10)
-    user.password=await bcrypt.hash(user.password,salt)
-    
-    await user.save()
+router.post("/signin", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-   const token= user.generateAuthToken()
-   const refreshToken = user.generateRefreshToken(); // Generate refresh token
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send("user already registered");
 
-    // Set the refresh token as a cookie
-    res.cookie('refreshToken', refreshToken, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production', // Set to true in production
-        sameSite: 'strict',
-        maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
-    });
-   res.header('x-auth-token',token).send( _.pick(user,['_id','name','email']))
-}) 
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
 
+  await user.save();
+
+  const token = user.generateAuthToken();
+  const refreshToken = user.generateRefreshToken(); // Generate refresh token
+
+  // Set the refresh token as a cookie
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production", // Set to true in production
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  });
+  res
+    .header("x-auth-token", token)
+    .send(_.pick(user, ["_id", "name", "email"]));
+});
 
 //login
-router.post('/login',async(req,res)=>{
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
 
-   const {email,password}=req.body
+  if (!email || !password)
+    return res.status(400).send("email and password required");
 
-   if(!email||!password) return res.status(400).send('email and password required')
-   
-   const user=await User.findOne({email})
-   if(!user) return res.status(400).send('Invalid email or password')
-   
-   const validPassword=await bcrypt.compare(password,user.password)
-   if(!validPassword) return res.status(400).send('Invalid email or password')
-   
-   const accessToken=user.generateAuthToken()
-   const refreshToken=user.generateRefreshToken()
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).send("Invalid email or password");
 
-   //refreshtoken
-   res.cookie('refreshToken',refreshToken,{
-      httpOnly:true,
-      secure: process.env.NODE_ENV === 'production', // Set to true in production
-      sameSite:'strict',
-      maxAge:7 * 24 * 60 * 60 * 1000
-   })
+  const validPassword = await bcrypt.compare(password, user.password);
+  if (!validPassword) return res.status(400).send("Invalid email or password");
 
-   res.header('x-auth-token',accessToken).send('user logged in successfully')
-   
+  const accessToken = user.generateAuthToken();
+  const refreshToken = user.generateRefreshToken();
 
-})
+  //refreshtoken
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production", // Set to true in production
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
 
-
+  res.header("x-auth-token", accessToken).send("user logged in successfully");
+});
 
 //route for refreshing the token
 
-router.post('/refresh-token',async(req,res)=>{
+router.post("/refresh-token", async (req, res) => {
+  const refreshToken = req.cookies.refreshToken;
+  console.log("Refresh Token:", refreshToken);
+  if (!refreshToken) return res.status(401).send("refresh token required!.");
 
-   const refreshToken = req.cookies.refreshToken
-   console.log('Refresh Token:', refreshToken);
-   if(!refreshToken) return res.status(401).send('refresh token required!.')
-   
-   try{
-        const decoded=jwt.verify(refreshToken,config.get('jwtRefreshKey'))
-         const user=await User.findById(decoded._id)
-         if (!user) return res.status(400).send('Invalid refresh token.')
-         const newAccessToken=user.generateAuthToken()
-         res.header('x-auth-token', newAccessToken).send('Token refreshed successfully.');
-   }catch(error){
-      console.error('Token verification error:', error);
-      res.status(403).send('Invalid or expired refresh token.');
-    }
-
-})
+  try {
+    //   const decoded=jwt.verify(refreshToken,config.get('jwtRefreshKey'))
+    const decoded = jwt.verify(refreshToken, jwtRefreshKey);
+    const user = await User.findById(decoded._id);
+    if (!user) return res.status(400).send("Invalid refresh token.");
+    const newAccessToken = user.generateAuthToken();
+    res
+      .header("x-auth-token", newAccessToken)
+      .send("Token refreshed successfully.");
+  } catch (error) {
+    console.error("Token verification error:", error);
+    res.status(403).send("Invalid or expired refresh token.");
+  }
+});
 
 //for loggingout
 
-router.post('/api/logout', (req, res) => {
-   res.clearCookie('refreshToken'); // Clear the refresh token cookie
-   res.send('Logged out successfully.');
+router.post("/api/logout", (req, res) => {
+  res.clearCookie("refreshToken"); // Clear the refresh token cookie
+  res.send("Logged out successfully.");
 });
 
-
 //export
-module.exports=router 
-
+module.exports = router;
